8c246a6a6302deb8f546505c8e3f957772d314a4
konoyaro <konoyaro@gmail.com>
2023-03-21 20:09:11 +0800

    tambah paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
 	<h1>Denjirooooo</h1>
+	<p>Lorem ipsum dolor sit amet, consectetur adipisicing, elit. Voluptatem consectetur iure sint laudantium magni tenetur delectus veritatis enim aut illum sed excepturi veniam, sapiente obcaecati a sequi. Modi eum, itaque.</p>
 </body>
 </html>
